fd5a6386c5db914f1f3dd434ac56af3fa50fdec1
thiago3100 <thfelipe31@gmail.com>
2022-06-09 12:28:20 -0300

    adicionando Foreach

---
+++ src/Foreach.java
@@ -0,0 +1,10 @@
+public class Foreach {
+    public static void main(String args[]) {
+        
+        String[] nomes = {"Thiago", "Felipe","Santos"};
+        
+        for(String name : nomes){
+            System.out.println(name);
+        }
+    }
+}
