ca1ae0e14d4d81b0b33d6d872ae345b24c872ab1
thiago3100 <thfelipe31@gmail.com>
2022-06-09 15:25:48 -0300

    adicionando autoboxing

---
+++ src/autoboxing.java
@@ -0,0 +1,14 @@
+public class autoboxing {
+
+    public static void main(String args[]) {
+     
+        Integer x = 555;
+        x++;
+        System.out.println(x);
+        
+        Boolean v = new Boolean ("true");
+        if(v){
+            System.out.print(v);
+        }
+    }
+}
