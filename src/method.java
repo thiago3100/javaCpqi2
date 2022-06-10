3fb09edc298c89ad35527243febe3f38a1fa8f1f
thiago3100 <thfelipe31@gmail.com>
2022-06-10 11:44:08 -0300

    adicionando metodos com retorno

---
+++ src/method.java
@@ -0,0 +1,10 @@
+public class method {
+    
+    int valor(int um, int dois){
+        if(um > dois){
+            return um;
+         }else{
+          return dois; 
+    }    
+ }
+}
---
+++ src/methodRetorno.java
@@ -0,0 +1,11 @@
+public class methodRetorno {
+
+    public static void main(String args[]) {
+        
+        method total = new method();
+        
+        int results = total.valor(20, 10);
+        
+        System.out.println(results);
+    }
+}
